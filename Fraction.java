a3f403289e19940500ae9fcad85f0267882e17b9
uvsq <moundjibl@hotmail.com>
2020-09-21 15:54:05 +0200

    sauvgarde

---
+++ Fraction.java
@@ -0,0 +1,57 @@
+import javax.lang.model.type.NullType;
+
+public class Fraction
+{
+    private int numerator;
+
+    public int getNumerator() {
+        return numerator;
+    }
+    public void setNumerator(int a) {
+        this.numerator = a;
+    }
+    public int getDenumerator() {
+        return denumerator;
+    }
+    public void setDenumerator(int b) {
+        this.denumerator = b;
+    }
+
+    private int denumerator;
+
+    public Fraction (int a,int b)
+    {
+        this.numerator=a;
+        if (b == 0)
+        {
+            throw new AssertionError();
+        }
+        this.denumerator=b;
+    }
+
+    public Fraction multiply (Fraction F1)
+    {
+        Fraction F= new Fraction(this.numerator*F1.getNumerator(), this.denumerator*F1.getDenumerator());
+        return F;
+    }
+
+    public Fraction add (Fraction F1)
+    {
+        Fraction F;
+        F= new Fraction( ((this.numerator*F1.getDenumerator())+(this.denumerator*F1.getNumerator())),
+                this.denumerator*F1.getDenumerator()
+        );
+        return F;
+    }
+    public Fraction subs (Fraction F1)
+    {
+        Fraction F;
+        F= new Fraction( ((this.numerator*F1.getDenumerator())-(this.denumerator*F1.getNumerator())),
+                this.denumerator*F1.getDenumerator()
+        );
+        return F;
+    }
+
+
+
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println(" HELLO WORLD GIVE ME A FRACTTION NUMBER");
+        Scanner sc =new Scanner(System.in);
+
+        int numerator,denumerator;
+
+        numerator=sc.nextInt();
+        denumerator=sc.nextInt();
+
+    }
+}
